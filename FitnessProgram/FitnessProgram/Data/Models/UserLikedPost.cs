fac141eb011394a205a86617d5d3545476c75176
viti00 <viti00@abv.bg>
2022-08-02 19:55:50 +0300

    Added data models

---
+++ FitnessProgram/FitnessProgram/Data/Models/UserLikedPost.cs
@@ -0,0 +1,15 @@
+﻿namespace FitnessProgram.Data.Models
+{
+    using Microsoft.AspNetCore.Identity;
+    using System.ComponentModel.DataAnnotations.Schema;
+
+    public class UserLikedPost
+    {
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(Post))]
+        public string PostId { get; set; }
+
+        public virtual Post Post { get; set; }
+    }
+}
---
+++ FitnessProgram/FitnessProgram/Models/Customer/CustomerFormModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using static FitnessProgram.Data.DataConstants;
+
+namespace FitnessProgram.Models.Customer
+{
+    public class CustomerFormModel
+    {
+        [Required]
+        [StringLength(CustomerConstants.FirstNameMaxLength, MinimumLength =CustomerConstants.FirstNameMinLength)]
+        public string FirstName { get; init; }
+
+        [Required]
+        [StringLength(CustomerConstants.LastNameMaxLength, MinimumLength =CustomerConstants.LastNameMinLength)]
+        public string LastName { get; init; }
+
+        [Range(CustomerConstants.AgeMinValue, CustomerConstants.AgeMaxValue)]
+        public int Age { get; init; }
+
+        [Range(typeof(decimal), CustomerConstants.WeightMinValue, CustomerConstants.WeightMaxValue)]
+        public decimal Weight { get; init; }
+
+        [Range(typeof(decimal), CustomerConstants.HeightMinValue, CustomerConstants.HeightMaxValue)]
+        public decimal Height { get; init; }
+
+        [Required]
+        [StringLength(CustomerConstants.DesiredResultMaxLength, MinimumLength =CustomerConstants.DesiredResultMinLength)]
+        public string DesiredResults { get; init; }
+    }
+}
